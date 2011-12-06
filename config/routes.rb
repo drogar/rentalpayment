eba30a6de38efec7c87f6871af0d88b8d7317c16
Brett G. Giles <brett.giles@drogar.com>
2011-12-06 14:16:56 -0700

    LANDLORD-2 Add user model, devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rentalpayment::Application.routes.draw do
+  devise_for :users
+
   resources :property_types
 
   resources :salutations
@@ -55,7 +57,9 @@ Rentalpayment::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-
+  
+  root :to => "people#index"
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
