5cea9958c9cd727bb1b1a6a83536b3140efa6416
Brett G. Giles <brett.giles@drogar.com>
2011-12-07 14:27:02 -0700

    LANDLORD-2 added mtm roles to users

---
+++ db/migrate/20111207211053_add_roles_users_join_table.rb
@@ -0,0 +1,14 @@
+class AddRolesUsersJoinTable < ActiveRecord::Migration
+  def up
+    create_table :roles_users, :id => false do |t|
+      t.integer :role_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :roles_users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111206212748) do
+ActiveRecord::Schema.define(:version => 20111207211053) do
 
   create_table "people", :force => true do |t|
     t.string   "email"
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(:version => 20111206212748) do
     t.string   "role_name"
     t.string   "description"
     t.boolean  "active"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "roles_users", :id => false, :force => true do |t|
+    t.integer  "role_id"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
